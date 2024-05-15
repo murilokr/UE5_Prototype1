de364abe23ad0db14c54bb3d8cc6ee65d8506f39
TyeBjorn <murilo.kr@hotmail.com>
2024-05-15 12:39:06 -0300

    Migrating to Character and C++

--- Source/Prototype1/Prototype1Character.cpp
+++ Source/Prototype1/Prototype1Character.cpp
@@ -10,6 +10,8 @@
 #include "EnhancedInputSubsystems.h"
 #include "InputActionValue.h"
 #include "Engine/LocalPlayer.h"
+#include <Kismet/KismetSystemLibrary.h>
+#include "GameFramework/CharacterMovementComponent.h"
 
 DEFINE_LOG_CATEGORY(LogTemplateCharacter);
 
@@ -42,6 +44,8 @@ void APrototype1Character::BeginPlay()
 {
 	// Call the base class  
 	Super::BeginPlay();
+
+	GetCharacterMovement()->SetMovementMode(EMovementMode::MOVE_Custom, 0);
 }
 
 //////////////////////////////////////////////////////////////////////////// Input
@@ -60,13 +64,18 @@ void APrototype1Character::SetupPlayerInputComponent(UInputComponent* PlayerInpu
 
 		// Looking
 		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &APrototype1Character::Look);
+
+		// Grabbing
+		EnhancedInputComponent->BindAction(GrabActionL, ETriggerEvent::Started, this, &APrototype1Character::GrabL);
+		EnhancedInputComponent->BindAction(GrabActionL, ETriggerEvent::Completed, this, &APrototype1Character::StopGrabL);
+		EnhancedInputComponent->BindAction(GrabActionR, ETriggerEvent::Started, this, &APrototype1Character::GrabR);
+		EnhancedInputComponent->BindAction(GrabActionR, ETriggerEvent::Completed, this, &APrototype1Character::StopGrabR);
 	}
 	else
 	{
 		UE_LOG(LogTemplateCharacter, Error, TEXT("'%s' Failed to find an Enhanced Input Component! This template is built to use the Enhanced Input system. If you intend to use the legacy system, then you will need to update this C++ file."), *GetNameSafe(this));
 	}
 }
-
 
 void APrototype1Character::Move(const FInputActionValue& Value)
 {
@@ -92,4 +101,213 @@ void APrototype1Character::Look(const FInputActionValue& Value)
 		AddControllerYawInput(LookAxisVector.X);
 		AddControllerPitchInput(LookAxisVector.Y);
 	}
+}
+
+void APrototype1Character::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	TickGrab(LeftHandData, DeltaSeconds);
+	TickGrab(RightHandData, DeltaSeconds);
+}
+
+void APrototype1Character::TickGrab(FHandsContextData& HandData, float DeltaSeconds)
+{
+	if (!HandData.IsGrabbing)
+	{
+		return;
+	}
+
+	const FVector HandLocation = HandData.GetHitLocation();
+	const FVector HandNormal = HandData.GetHitNormal();
+	const FRotator GrabRot = HandNormal.ToOrientationRotator();
+
+	// TODO Optimize
+	const float MinSearchDist = 0.2125 * 100.0f;
+	const float MaxSearchDist = 2.0f * 100.0f;
+	const FVector TraceStart = FirstPersonCameraComponent->GetComponentLocation() + FirstPersonCameraComponent->GetForwardVector() * FVector(MinSearchDist);
+	const FVector TraceEnd = FirstPersonCameraComponent->GetComponentLocation() + FirstPersonCameraComponent->GetForwardVector() * FVector(MaxSearchDist);
+	const FVector TraceDir = TraceEnd - TraceStart;
+	// End of TODO Optimize
+	
+	const FVector GrabTargetPosition = HandData.GetGrabPosition(TraceStart, TraceDir);
+
+	// Debugs
+	/** GrabRot relative to HandLocation */
+	DrawDebugCoordinateSystem(GetWorld(), HandLocation, GrabRot, 10.0f, false, -1.0f, 0, 1.0f);
+
+	/** HandLocation */
+	DrawDebugSphere(GetWorld(), HandLocation, 50.0f, 6, FLinearColor(1.0f, 0.39f, 0.87f, 1.0f).ToFColorSRGB());
+
+	/** HandNormal pointing outwards from HandLocation */
+	DrawDebugDirectionalArrow(GetWorld(), HandLocation, HandLocation + (HandNormal * 12.0f), 1.0f, FLinearColor(0.18f, 0.57f, 1.0f, 1.0f).ToFColorSRGB(), false, -1.0f, 0, 1.0f);
+
+	/** Arrow pointing from HandLocation to GrabTargetPosition */
+	DrawDebugDirectionalArrow(GetWorld(), HandLocation, GrabTargetPosition, 1.0f, FLinearColor(0.46f, 1.0f, 0.15f, 1.0f).ToFColorSRGB(), false, -1.0f, 0, 1.0f);
+	// End of Debugs
+
+	// This effectively only moves the Hand visual.
+	//LHCollision.SetRelativeLocation(HandData.LocalHitLocation);
+	const FVector DragOffset = HandLocation - GrabTargetPosition;
+	SetActorLocation(GetActorLocation() + DragOffset);
+
+
+	// When we want to GRAB and MOVE an object. do this. Also consider adding a PhysicsHandle instead, if the object is grabbable.
+	//const float DragForce = 500.0f;
+	//const FVector MoveObjectVectorWithForce = (GrabTargetPosition - HandLocation) * (DragForce / DeltaSeconds);
+}
+
+void APrototype1Character::Grab(int HandIndex)
+{
+	FHandsContextData& HandData = (HandIndex == 0) ? RightHandData : LeftHandData;
+	if (HandData.IsGrabbing)
+	{
+		// Something went wrong.
+		return;
+	}
+
+	const float MinSearchDist = 0.2125 * 100.0f;
+	const float MaxSearchDist = 2.0f * 100.0f;
+	const FVector TraceStart = FirstPersonCameraComponent->GetComponentLocation() + FirstPersonCameraComponent->GetForwardVector() * FVector(MinSearchDist);
+	const FVector TraceEnd = FirstPersonCameraComponent->GetComponentLocation() + FirstPersonCameraComponent->GetForwardVector() * FVector(MaxSearchDist);
+	const FVector TraceDir = TraceEnd - TraceStart;
+	const float TraceLength = TraceDir.SquaredLength();
+
+	// You can use FCollisionQueryParams to further configure the query
+	// Here we add ourselves to the ignored list so we won't block the trace
+	FCollisionQueryParams QueryParams;
+	QueryParams.AddIgnoredActor(this);
+
+	FHitResult HitResult;
+	GetWorld()->LineTraceSingleByChannel(HitResult, TraceStart, TraceEnd, ECollisionChannel::ECC_PhysicsBody, QueryParams);
+	DrawDebugLine(GetWorld(), TraceStart, TraceEnd, HitResult.bBlockingHit ? FColor::Blue : FColor::Red, false, 2.5f, 0, 1.25f);
+
+	// Nothing was hit.
+	if (!HitResult.bBlockingHit)
+	{
+		return;
+	}
+
+	HandData.IsGrabbing = true;
+	HandData.HitResult = HitResult;
+
+	// Calculate GrabPositionT
+	HandData.GrabPositionT = 0.0f;
+	const FVector PositionDir = HitResult.Location - TraceStart;
+	if (TraceDir.Dot(PositionDir) >= 0.0f)
+	{
+		// HitResult.Location is inline with TraceDir.
+		HandData.GrabPositionT = FMath::Clamp(PositionDir.SquaredLength() / TraceLength, 0.0f, 1.0f);
+	}
+
+	const FTransform HitBoneWorldToLocalTransform = HitResult.Component->GetSocketTransform(HitResult.BoneName).Inverse();
+	HandData.LocalHitLocation = HitBoneWorldToLocalTransform.TransformPosition(HitResult.Location);
+	HandData.LocalHitNormal = HitBoneWorldToLocalTransform.TransformVectorNoScale(HitResult.Normal);
+
+	// Need to enable this later.
+	//LHCollision.SetRelativeLocation(HandData.LocalHitLocation);
+	//OnStartGrabbing();
+}
+
+void APrototype1Character::StopGrabbing(int HandIndex)
+{
+	FHandsContextData& HandData = (HandIndex == 0) ? RightHandData : LeftHandData;
+
+	if (!HandData.IsGrabbing)
+	{
+		// We weren't grabbing anything.
+		return;
+	}
+
+	HandData.IsGrabbing = false;
+}
+
+const FVector APrototype1Character::GetHitLocation(int HandIndex)
+{
+	FHandsContextData HandData = (HandIndex == 0) ? RightHandData : LeftHandData;
+
+	return HandData.GetHitLocation();;
+}
+
+const FVector APrototype1Character::GetHitNormal(int HandIndex)
+{
+	FHandsContextData HandData = (HandIndex == 0) ? RightHandData : LeftHandData;
+
+	return HandData.GetHitNormal();
+}
+
+const FRotator APrototype1Character::GetHandRotation(int HandIndex)
+{
+	FHandsContextData HandData = (HandIndex == 0) ? RightHandData : LeftHandData;
+
+	return HandData.GetHandRotation();
+}
+
+const bool APrototype1Character::IsGrabbing()
+{
+	return LeftHandData.IsGrabbing || RightHandData.IsGrabbing;
+}
+
+void APrototype1Character::GrabR(const FInputActionValue& Value)
+{
+	Grab(0);
+}
+
+void APrototype1Character::StopGrabR(const FInputActionValue& Value)
+{
+	StopGrabbing(0);
+}
+
+void APrototype1Character::GrabL(const FInputActionValue& Value)
+{
+	Grab(1);
+}
+
+void APrototype1Character::StopGrabL(const FInputActionValue& Value)
+{
+	StopGrabbing(1);
+}
+
+
+FVector FHandsContextData::GetGrabPosition(const FVector TraceStart, const FVector TraceDir)
+{
+	return TraceStart + TraceDir * GrabPositionT;
+}
+
+FVector FHandsContextData::GetHitLocation()
+{
+	if (!HitResult.Component.IsValid())
+	{
+		return FVector();
+	}
+
+	const FTransform HitBoneLocalToWorldTransform = HitResult.Component->GetSocketTransform(HitResult.BoneName);
+
+	return HitBoneLocalToWorldTransform.TransformPosition(LocalHitLocation);
+}
+
+FVector FHandsContextData::GetHitNormal()
+{
+	if (!HitResult.Component.IsValid())
+	{
+		return FVector();
+	}
+
+	const FTransform HitBoneLocalToWorldTransform = HitResult.Component->GetSocketTransform(HitResult.BoneName);
+
+	return HitBoneLocalToWorldTransform.TransformVectorNoScale(LocalHitNormal);
+}
+
+FRotator FHandsContextData::GetHandRotation()
+{
+	const FVector HandNormal = GetHitNormal();
+	const FRotator HandRotation = FRotationMatrix::MakeFromY(HandNormal).Rotator();
+	
+	// If right hand, we might want to Inverse the Rotator.
+
+	// Debug
+	DrawDebugCoordinateSystem(GEngine->GetWorld(), GetHitLocation(), HandRotation, 10.0f, false, -1.0f, 0, 1.0f);
+
+	// Hand bones are oriented towards Y, which is why we don't get OrientationVector.
+	return HandRotation;
 }
--- Source/Prototype1/Prototype1Character.h
+++ Source/Prototype1/Prototype1Character.h
@@ -16,10 +16,46 @@ struct FInputActionValue;
 
 DECLARE_LOG_CATEGORY_EXTERN(LogTemplateCharacter, Log, All);
 
+
+USTRUCT(BlueprintType)
+struct FHandsContextData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	bool IsGrabbing;
+
+	// Maybe we'll remove this.
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	float GrabPositionT;
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	FHitResult HitResult;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	FVector LocalHitLocation;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	FVector LocalHitNormal;
+
+	// Grab Target Position.
+	FVector GetGrabPosition(const FVector TraceStart, const FVector TraceDir);
+
+	// Hand Location
+	FVector GetHitLocation();
+
+	FVector GetHitNormal();
+
+	FRotator GetHandRotation();
+};
+
+
 UCLASS(config=Game)
 class APrototype1Character : public ACharacter
 {
 	GENERATED_BODY()
+
+public:
 
 	/** Pawn mesh: 1st person view (arms; seen only by self) */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Mesh, meta = (AllowPrivateAccess = "true"))
@@ -36,36 +72,84 @@ class APrototype1Character : public ACharacter
 	/** Move Input Action */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess = "true"))
 	UInputAction* MoveAction;
-	
-public:
-	APrototype1Character();
 
-protected:
-	virtual void BeginPlay();
-
-public:
-		
 	/** Look Input Action */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
 	class UInputAction* LookAction;
 
+	/** Grab Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* GrabActionL;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* GrabActionR;
+	
+	APrototype1Character();
+
+	/** Returns Mesh1P subobject **/
+	USkeletalMeshComponent* GetMesh1P() const { return Mesh1P; }
+	/** Returns FirstPersonCameraComponent subobject **/
+	UCameraComponent* GetFirstPersonCameraComponent() const { return FirstPersonCameraComponent; }
+
+	/** Hand Utility Functions */
+	// Hand Location
+	UFUNCTION(BlueprintPure)
+	const FVector GetHitLocation(int HandIndex);
+
+	// Hand Normal
+	UFUNCTION(BlueprintPure)
+	const FVector GetHitNormal(int HandIndex);
+
+	// Hand Rotation
+	UFUNCTION(BlueprintPure)
+	const FRotator GetHandRotation(int HandIndex);
+
+	// IsGrabbing
+	UFUNCTION(BlueprintPure)
+	const bool IsGrabbing();
+	/** End of Hand Utility Functions */
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = PhysicalArms)
+	float ArmsLengthUnits = 54.5f; //45
+
 protected:
+	virtual void BeginPlay();
+
+	virtual void Tick(float DeltaSeconds) override;
+
 	/** Called for movement input */
 	void Move(const FInputActionValue& Value);
 
 	/** Called for looking input */
 	void Look(const FInputActionValue& Value);
 
-protected:
+	/** Called for grabbing input Right */
+	void GrabR(const FInputActionValue& Value);
+
+	/** Called for grabbing input Right */
+	void StopGrabR(const FInputActionValue& Value);
+
+	/** Called for grabbing input Left */
+	void GrabL(const FInputActionValue& Value);
+
+	/** Called for grabbing input Left */
+	void StopGrabL(const FInputActionValue& Value);
+
+	/** Called for grabbing input */
+	void Grab(const int HandIndex);
+
+	void TickGrab(FHandsContextData& HandData, float DeltaSeconds);
+
+	/** Called for grabbing input */
+	void StopGrabbing(const int HandIndex);
+
 	// APawn interface
 	virtual void SetupPlayerInputComponent(UInputComponent* InputComponent) override;
 	// End of APawn interface
 
-public:
-	/** Returns Mesh1P subobject **/
-	USkeletalMeshComponent* GetMesh1P() const { return Mesh1P; }
-	/** Returns FirstPersonCameraComponent subobject **/
-	UCameraComponent* GetFirstPersonCameraComponent() const { return FirstPersonCameraComponent; }
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FHandsContextData LeftHandData;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FHandsContextData RightHandData;
 };
 
