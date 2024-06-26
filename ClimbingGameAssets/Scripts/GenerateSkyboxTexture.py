d8c1c26066a7fd949b1f17a69ee033ff14bab042
TyeBjorn <murilo.kr@hotmail.com>
2024-06-26 20:00:21 -0300

    Updated skybox texture script for batch generation

--- ClimbingGameAssets/Scripts/GenerateSkyboxTexture.py
+++ ClimbingGameAssets/Scripts/GenerateSkyboxTexture.py
@@ -2,6 +2,8 @@ from PIL import Image, ImageOps
 import os
 
 output_folder = '../Output/'
+texture_name = "Sky_MountainSide_Alt_"
+
 
 def paste_image(input_image, output_image, position, flip_horizontal=False, rotate_180=False):
     # Flip horizontally if specified
@@ -18,7 +20,7 @@ def paste_image(input_image, output_image, position, flip_horizontal=False, rota
     # Paste the input image onto the output image at the specified position
     output_image.paste(input_image, position, input_image)
 
-def process_images(layer_image_paths, individual_image_paths, layer_position, individual_positions, output_image_path):
+def process_images(skybox_layer_faces, individual_skybox_faces, layer_position, individual_positions, frame_number, output_image_path):
     # Check if the output image file already exists
     if os.path.exists(output_image_path):
         # Open existing output image for editing
@@ -27,14 +29,15 @@ def process_images(layer_image_paths, individual_image_paths, layer_position, in
         # Create the output image with the desired size (2048x2048)
         output_image = Image.new('RGBA', (2048, 2048))
     
-    # Process layer images
-    if layer_image_paths:
+    # Process skybox layer faces
+    if skybox_layer_faces:
         # Create a temporary layer image
         temp_layer_image = Image.new('RGBA', (2048, 512))
         
-        # Iterate over layer image paths and paste onto temp_layer_image
-        for i, image_path in enumerate(layer_image_paths):
-            input_image = Image.open(output_folder + image_path)
+        # Iterate over layer skybox faces and paste onto temp_layer_image
+        for i, skybox_face in enumerate(skybox_layer_faces):
+            skybox_face_path = output_folder + "Sky_" + skybox_face + frame_number + ".bmp"
+            input_image = Image.open(skybox_face_path)
             position = ((512 * i), 0) # (0, 0)
             paste_image(input_image, temp_layer_image, position, flip_horizontal=True)
         
@@ -42,10 +45,11 @@ def process_images(layer_image_paths, individual_image_paths, layer_position, in
         output_image.paste(temp_layer_image, layer_position)
     
     # Process individual images
-    if individual_image_paths:
-        # Iterate over individual image paths and paste onto output_image
-        for i, image_path in enumerate(individual_image_paths):
-            input_image = Image.open(output_folder + image_path)
+    if individual_skybox_faces:
+        # Iterate over individual skybox faces and paste onto output_image
+        for i, skybox_face in enumerate(individual_skybox_faces):
+            skybox_face_path = output_folder + "Sky_" + skybox_face + frame_number + ".bmp"
+            input_image = Image.open(skybox_face_path)
             position = individual_positions[i]
             
             # Flip horizontally and rotate 180 degrees before pasting
@@ -54,14 +58,17 @@ def process_images(layer_image_paths, individual_image_paths, layer_position, in
     # Save the output image
     output_image.save(output_image_path)
 
-# Example usage:
+
+
 if __name__ == "__main__":
-    layer_image_paths = ['Sky_Back0001.bmp', 'Sky_Left0001.bmp', 'Sky_Front0001.bmp', 'Sky_Right0001.bmp']
+    skybox_layer_faces = ["Back", "Left", "Front", "Right"]
     layer_position = (0, 768)
     
-    individual_image_paths = ['Sky_Up0001.bmp', 'Sky_Down0001.bmp']
+    individual_skybox_faces = ["Up", "Down"]
     individual_positions = [(512, 256), (512, 1280)]
     
-    output_image_path = '../Textures/output_image.png'
+    frames = ["0001", "0002", "0003", "0004", "0005", "0006", "0007", "0008", "0009", "0010", "0011", "0012", "0013", "0014", "0015", "0016", "0017", "0018", "0019", "0020", "0021"]
     
-    process_images(layer_image_paths, individual_image_paths, layer_position, individual_positions, output_image_path)
+    for frame_number in frames:
+        output_image_path = "../Textures/" + texture_name + frame_number + ".png"
+        process_images(skybox_layer_faces, individual_skybox_faces, layer_position, individual_positions, frame_number, output_image_path)
